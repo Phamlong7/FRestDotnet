0184e9c206b9705d2a2d3e0932b4749136338afd
Phamlong7 <88232978+Phamlong7@users.noreply.github.com>
2024-10-16 18:07:35 +0700

    Fix

--- Restaurant/Program.cs
+++ Restaurant/Program.cs
@@ -8,7 +8,7 @@ using Restaurant.Areas.Admin;
 var builder = WebApplication.CreateBuilder(args);
 
 // Configure services before building the app
-
+builder.Services.AddLogging();
 // Connection Database 
 builder.Services.AddDbContext<DataContext>(options =>
 {
@@ -38,10 +38,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddTransient<IFileService, FileService>(); //file service
 builder.Services.AddSingleton<ConstantHelper>();
 builder.Services.AddTransient<SendMail>();
-builder.Services.AddSession(options =>         //session
-{
-    options.IdleTimeout = TimeSpan.FromHours(1);
-});
+builder.Services.AddScoped<SendMail>();
 
 // Configure logging before building the app
 builder.Logging.AddConfiguration(builder.Configuration.GetSection("Logging"));
@@ -58,8 +55,7 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthentication(); // Ensures authentication is enabled  
-app.UseAuthorization();  // Ensures authorization is enabled
-app.UseSession(); 
+app.UseAuthorization();  // Ensures authorization is enabled  
 
 // Route Controller  
 #pragma warning disable
