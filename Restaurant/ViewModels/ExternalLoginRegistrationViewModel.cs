38258b78e0086a3dd1f6d23a862964e8b702db04
Phamlong7 <88232978+Phamlong7@users.noreply.github.com>
2024-10-25 00:14:11 +0700

    Update and fix bug

---
+++ Restaurant/ViewModels/ExternalLoginRegistrationViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Restaurant.ViewModels
+{
+    public class ExternalLoginRegistrationViewModel
+    {
+        [Required]
+        [StringLength(50)]
+        public string UserName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
